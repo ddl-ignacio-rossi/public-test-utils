77944a3a40a5a71c02725c9ef7a8110f994fc79a
Ignacio Rossi <ignacio.rossi@dominodatalab.com>
2025-07-01 12:31:23 -0300

    Colors

--- check_persistence.sh
+++ check_persistence.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-echo "Checking tornado from requirements.txt"
-python -c "import tornado; print(tornado.version)" || echo "Tornado is not installed or not found."
+green() {
+  echo -en "\033[0;32m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
 
-echo "Checking pipdeptree"
+red() {
+  echo -en "\033[0;31m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
+
+blue() {
+  echo -en "\033[0;34m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
+
+blue "Checking tornado from requirements.txt"
+python -c "import tornado; print(tornado.version)" || red "Tornado is not installed or not found."
+
+blue "Checking pipdeptree"
 which pipdeptree
 if ! pipdeptree >/dev/null; then
-  echo "pipdeptree is not installed."
+  red "pipdeptree is not installed."
 else
-  echo "pipdeptree is installed."
+  green "pipdeptree is installed."
 fi
 
-echo "Checking R random package"
-R --no-save -e 'library(random); random::randomNumbers()'
+blue "Checking R random package"
+if ! R --no-save -e 'library(random); random::randomNumbers()'; then
+  red "R random package is not installed or not found."
+else
+  green "R random package is installed."
+fi
 
-echo "Checking contents in home"
-cat ~/persistence-test
+blue "Checking contents in home"
+cat ~/persistence-test || red "Failed to show ~/persistence-test"
--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-echo "Installing pipdeptree with pip: $(which pip)"
+green() {
+  echo -en "\033[0;32m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
+
+red() {
+  echo -en "\033[0;31m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
+
+blue() {
+  echo -en "\033[0;34m"
+  echo -n "$@"
+  echo -e "\033[0m"
+}
+
+blue "Installing pipdeptree with pip: $(which pip)"
 pip install pipdeptree
 
-echo "Installing random package in R"
+blue "Installing random package in R"
 R --no-save -e 'install.packages("random")'
 
-echo "Creating file in home"
-date >~/persistence-test
+the_date=$(date)
+blue "Creating file in home with date $the_date"
+echo "$the_date" >~/persistence-test
